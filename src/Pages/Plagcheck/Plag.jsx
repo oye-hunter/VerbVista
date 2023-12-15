224e8d661c13c563b0559397dfa7f56cddfd7a90
MalikHammadZahid <hm0167968@gmail.com>
2023-12-15 13:37:13 +0500

    hi

--- src/Pages/Plagcheck/Plag.jsx
+++ src/Pages/Plagcheck/Plag.jsx
@@ -1,12 +1,7 @@
-// MainPage.js
 
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './Plag.css';
-import upload from "../../assets/cloud-computing.png";
 import Header from '../../Components/Header/Header';
 import Faq from '../../Components/Faq/Faq';
-import Plagchkr from '../../Components/Plagchkr/Plagchkr';
+
 import Footer from '../../Components/Footer/Footer';
 import Summarizer from "../../Components/Summarizer/Summarizer"
 
@@ -14,13 +9,7 @@ const Plag = () => {
   return(
     <>
     <Header/>
-<<<<<<< HEAD
     <Summarizer  head={"Plagiarism Checker"} button={"Check"} placeholder={"Enter Text "} />
-=======
-    {/* <Summarizer  head={"Plagirisim Checker"} button={"Check"}  /> */}
-<Plagchkr/>
-
->>>>>>> 7ce8a314630abbb78357051a4964bb69b89c20e9
     <Faq/>
     <Footer/>
     </>
