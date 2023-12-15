d130ccfab46cdc2d06d626af2b971e6f58ccc872
MalikHammadZahid <hm0167968@gmail.com>
2023-12-15 13:36:54 +0500

    hi

--- src/Pages/Plagcheck/Plag.jsx
+++ src/Pages/Plagcheck/Plag.jsx
@@ -6,6 +6,7 @@ import './Plag.css';
 import upload from "../../assets/cloud-computing.png";
 import Header from '../../Components/Header/Header';
 import Faq from '../../Components/Faq/Faq';
+import Plagchkr from '../../Components/Plagchkr/Plagchkr';
 import Footer from '../../Components/Footer/Footer';
 import Summarizer from "../../Components/Summarizer/Summarizer"
 
@@ -13,7 +14,13 @@ const Plag = () => {
   return(
     <>
     <Header/>
+<<<<<<< HEAD
     <Summarizer  head={"Plagiarism Checker"} button={"Check"} placeholder={"Enter Text "} />
+=======
+    {/* <Summarizer  head={"Plagirisim Checker"} button={"Check"}  /> */}
+<Plagchkr/>
+
+>>>>>>> 7ce8a314630abbb78357051a4964bb69b89c20e9
     <Faq/>
     <Footer/>
     </>
